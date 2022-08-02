d7e19f080dca352cb4c2743b16a69d75cd51b02e
Lynn Kale <lynn@homevalet.co>
2022-08-02 10:45:34 -0700

    Updated top bar to use top bar material composable.

--- app/src/main/java/com/lynnkale/todoallthethings/ui/TopBar.kt
+++ app/src/main/java/com/lynnkale/todoallthethings/ui/TopBar.kt
@@ -1,24 +1,20 @@
 package com.lynnkale.todoallthethings.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.lynnkale.todoallthethings.ui.theme.defaultSpace
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
-    Row(
-        modifier = Modifier,
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Title(text = "To Do ALL THE THINGS!", modifier = Modifier.padding(defaultSpace))
-    }
-
+fun TopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = { Title(text = "To Do ALL THE THINGS!", modifier = Modifier) },
+        modifier = modifier,
+        navigationIcon = {},
+        actions = {},
+    )
 }
 
 @Preview
