8cbfea2fc8cb0bf2e2160c03d2be25e795010551
Lynn Kale <lynn@homevalet.co>
2022-08-10 09:29:15 -0700

    update keystore directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     dependencies {
         classpath(kotlin("gradle-plugin", kotlin_version))
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
